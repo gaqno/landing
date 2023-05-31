fbfd0af37e06ff9dc473041020ee46cb4035eaa9
gaqno <gabriel.aquino@outlook.com>
2023-05-30 22:48:12 -0300

    Update contact section text and position

--- pages/index.vue
+++ pages/index.vue
@@ -374,13 +374,13 @@
 
       <div class="my-32">
         <div id="contact" data-aos="fade-right" class="lg:col-span-10 lg:col-start-1 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl">
-          <div class="relative max-w-md space-y-6 px-6 py-12 sm:max-w-3xl sm:py-16 lg:col-span-6 lg:col-start-1 lg:max-w-none lg:p-0">
+          <div class="relative max-w-md space-y-6 px-6 py-12 sm:max-w-3xl sm:py-16 lg:col-span-6 lg:col-start-2 lg:max-w-none lg:p-0">
             <h2 class="text-left text-2xl font-semibold leading-8 text-green-500">
               # Contato
             </h2>
             <span>
-              Entre em contato comigo através dos meios
-              <div>
+              Entre em contato através dos meios abaixo.
+              <div class="mt-4">
                 <a class="block hover:text-indigo-300" href="mailto:gabriel.aquino@outlook.com">
                   <Icon name="line-md:email" class="mr-2" />
                   gabriel.aquino@outlook.com
